55c1252578255dc679a1727b4b980c5a969c2a09
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-28 01:54:10 +0300

    feat(blog index view): add header, blog cards, buttons

--- obj/Debug/net10.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net10.0/Blog.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6bf1547f315fade33e80b2e7c47965a82446027")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
