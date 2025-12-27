f6bf1547f315fade33e80b2e7c47965a82446027
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-28 01:25:56 +0300

    feat(program): define the index blog route

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -2,42 +2,11 @@
 
 namespace Blog.Controllers;
 
-public class BlogsController : Controller
+public class BlogController : Controller
 {
-    private record BlogListItem(int Id, string Title, string Author);
-
+    // GET
     public IActionResult Index()
     {
-        var blogs = new List<BlogListItem>
-        {
-            new(1, "Building a Modern ASP.NET Core Blog", "Alice Writer"),
-            new(2, "Dark Theme UX Tips for Developers", "Boris Admin"),
-            new(3, "Why Dependency Injection Matters", "Carla Viewer")
-        };
-
-        return View(blogs);
-    }
-
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    public IActionResult Details(int id)
-    {
-        ViewData["Placeholder"] = id;
-        return View();
-    }
-
-    public IActionResult Edit(int id)
-    {
-        ViewData["Placeholder"] = id;
-        return View();
-    }
-
-    public IActionResult Delete(int id)
-    {
-        ViewData["Placeholder"] = id;
-        return View();
+        return View("Index");
     }
 }
