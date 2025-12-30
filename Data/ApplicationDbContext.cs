76a4e6d782e22d6344b71f4dc80882beef5da7f3
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-30 21:08:08 +0300

    feat(application db context): set the bridge between code and database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Blog.Models;
+using BlogEntity = Blog.Models.Blog;
 
-namespace Blog.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+namespace Blog.Data
 {
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<BlogEntity> Blogs => Set<BlogEntity>();
+    }
 }
