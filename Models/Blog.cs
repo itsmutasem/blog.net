07048aa10f546f0f7a6483429590347a402c8cde
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-30 21:00:13 +0300

    feat(blog model): set the blog model columns

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,6 +1,26 @@
-﻿namespace Blog.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models;
 
 public class Blog
 {
+    public int Id { get; set; }
+    
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    
+    [Required]
+    public string Description { get; set; } = string.Empty;
+    
+    [Required]
+    public string Category { get; set; } = string.Empty;
+    
+    // Foreign Key
+    [ForeignKey("User")]
+    public int UserId { get; set; }
+    
+    // Navigation
+    public User User { get; set; } = null!;
     
 }
