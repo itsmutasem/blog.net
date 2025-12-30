9ec3a8dda34ca8ef56b9e40ea4b5ee28a3502eb7
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-30 20:56:08 +0300

    feat(user model): set the user model columns

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,23 @@
-﻿namespace Blog.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models;
 
 public class User
 {
+    public int Id { get; set; }
     
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    
+    [Required]
+    public string Password { get; set; } = string.Empty;
+    
+    [Required]
+    public string Role { get; set; } = "User"; // Admin or User
+    
+    // Navigation
+    public ICollection<Blog> Blogs { get; set; } = new List<Blog>();
 }
