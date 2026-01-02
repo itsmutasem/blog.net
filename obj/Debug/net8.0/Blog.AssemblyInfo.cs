44ad44f38e4ae1ad68f63cfb614eb0fea74e7fc0
Mutasem Mustafa <itsmutasem@gmail.com>
2026-01-02 18:22:26 +0300

    style: show page

--- obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50110ab49d19d3021811e4a7a259f1cf55c37d73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4a1d914ffdbf20fddbeb8bfe682cfcf79ccda7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
